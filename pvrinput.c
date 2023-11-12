bf0cf65866c7252a54dc23f77282306720787019
Manuel Reimer <manuel.reimer@gmx.de>
2023-11-12 16:21:53 +0100

    Version 20231112

--- pvrinput.c
+++ pvrinput.c
@@ -6,7 +6,7 @@
 #endif
 #endif
 
-static const char *VERSION        = "2015-11-07";
+static const char *VERSION        = "2023-11-12";
 static const char *DESCRIPTION    = tr("use Hauppauge PVR as input device");
 static const char *MAINMENUENTRY  = tr("PVR picture settings");
 
